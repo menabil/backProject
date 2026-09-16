8d8ae095e9774edbd9bb3e603a3f3fbf3ddf3268
menabil <nabil99cc@gmail.com>
2026-09-16 23:14:00 +0600

    add transporter into utils

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,15 +5,7 @@ const otpGenerator = require("otp-generator");
 const User = require("../models/userSchema");
 const { sendOtp, login } = require("../controller/authController");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "nabil1000cc@gmail.com",
-    pass: "ecqdooxiqrurlfgm",
-  },
-});
+
 
 router.post("/sendotp", sendOtp);
 
---
+++ utils/transporter.js
@@ -0,0 +1,11 @@
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "nabil1000cc@gmail.com",
+    pass: "ecqdooxiqrurlfgm",
+  },
+});
+
+module.exports = transporter;
